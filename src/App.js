7fc13999de7f008432ebecb4162412e85dbb2565
tirth765 <tirth.patel6521@gmail.com>
2024-11-21 08:35:57 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
+        <div> </div>
       </header>
     </div>
   );
