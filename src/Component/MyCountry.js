44adbe298995013fedcd9155043b5362081d02f0
tirth765 <tirth.patel6521@gmail.com>
2024-11-22 09:21:26 +0530

    MyCountry

--- src/Component/MyCountry.js
+++ src/Component/MyCountry.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 
 export default class MyCountry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            country: 'India',
+            Population: '135 Billion'
+        }
+    }
+
+    handleCountry = () => {
+        this.setState({
+            country: 'USA',
+            Population: '30 Billion'
+        })
+    }
+
     render() {
         return (
             <>
+
             <h1>Class Base Component</h1>
 
             <div>
-                MyCountry
+                MyCountry {this.state.country}
             </div>
-            
+
+            <h4>Population {this.state.Population} </h4>
+
+            <button onClick={this.handleCountry}>Change Country</button>
+
             </>
         );
     }
