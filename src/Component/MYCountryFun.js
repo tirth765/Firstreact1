3c6509373fc9137121cd81445e91a7d34be9fc26
tirth765 <tirth.patel6521@gmail.com>
2024-11-23 09:25:41 +0530

    Toogle

--- src/Component/MYCountryFun.js
+++ src/Component/MYCountryFun.js
@@ -1,17 +1,59 @@
-import React from 'react'
+// import React , { useState } from 'react'
+
+// export default function MYCountryFun() {
+
+//     const[country, setCountry] = useState(false);
+//     const[Population, setPopulation] = useState (false);
+
+//     const handleCountry = () => {
+      
+//       setCountry(!country);
+//       setPopulation(!Population);
+//     }
+    
+//   return (
+//     <>
+//     <h1>Function Base Component</h1>
+//     <div>MYCountry {!country ? 'India' : 'USA'}</div>
+
+//     <h4>Population {!Population ? '30 Billion' : '50 Billion'} </h4>
+
+//     <button onClick={handleCountry}>Change Country</button>
+    
+
+//     </>
+//   )
+// }
+
+
+import React , { useState } from 'react'
 
 export default function MYCountryFun() {
-    // const[props, setCountry] = 
-        
-    // }
 
-    
+    const[country, setCountry] = useState('India');
+    const[Population, setPopulation] = useState ('30 Billion');
+
+    const handleCountry = () => {
+      if(country === 'India') {
+        setCountry('USA');
+        setPopulation('50 Billion');
+      } else {
+        setCountry('India');
+        setPopulation('30 Billion');
+      }
+     
+    }
     
   return (
     <>
     <h1>Function Base Component</h1>
+    <div>MYCountry {country}</div>
 
-    <div>MYCountryFun</div>
+    <h4>Population {Population} </h4>
+
+    <button onClick={handleCountry}>Change Country</button>
+    
+
     </>
   )
 }
