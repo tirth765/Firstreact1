8fa9e03e15b09ac353e3226810e38e06dc3077b1
tirth765 <tirth.patel6521@gmail.com>
2024-11-24 21:00:07 +0530

    Props

--- src/Component/MyCountry.js
+++ src/Component/MyCountry.js
@@ -1,36 +1,64 @@
-import React, { Component } from 'react';
-
 export default class MyCountry extends Component {
     constructor(props) {
         super(props);
+        
         this.state = {
-            country: 'India',
-            Population: '135 Billion'
+            name: true,
+            Population : true
         }
     }
 
-    handleCountry = () => {
-        this.setState({
-            country: 'USA',
-            Population: '30 Billion'
-        })
-    }
+HandleCounty = () => {
+    this.setState((prevState) => ({
+        name: !prevState.name,
+        Population: !prevState.Population
 
-    render() {
-        return (
-            <>
-
-            <h1>Class Base Component</h1>
-
-            <div>
-                MyCountry {this.state.country}
-            </div>
-
-            <h4>Population {this.state.Population} </h4>
-
-            <button onClick={this.handleCountry}>Change Country</button>
-
-            </>
-        );
-    }
+      }));
 }
+
+render() {
+    return (
+        <div>
+            <h2>Class Based Component</h2>
+            <div>MyCountry  {this.state.name ? 'India' : 'USA'}</div>
+            <div>Population  {this.state.Population ? '100 Billion' : '10 Billion'}</div>
+            <br></br>
+            <button onClick={this.HandleCounty}>Change Country</button>
+        </div>
+      );
+    }
+}
+
+
+
+
+// import React, { Component } from 'react';
+
+// export default class MyCountry extends Component {
+//     constructor(props) {
+//         super(props);
+        
+//         this.state = {
+//             name : 'India',
+//             Population : '142 Billion'
+//         }
+//     }
+
+//     HandleCounty = () => {
+//         this.setState({
+//              name : 'USA',
+//             Population : '14 Billion'
+//         })
+//         if(this.state.name === 'India'){
+//             this.setState({
+//                      name : 'USA',
+//                     Population : '14 Billion'
+//                 })
+//         }else{
+//             this.setState({
+//                 name : 'India',
+//                 Population : '142 Billion'
+//             })
+//         }
+//     }
+// }
